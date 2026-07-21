8d99027ca8f0f346a74fcc3bbbc8e43c0a712560
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-07-21 14:36:08 +0300

    Question9

--- Assginment2/Program.cs
+++ Assginment2/Program.cs
@@ -106,6 +106,21 @@ namespace Assginment2
             //    Console.WriteLine("Short Book");
             //}
             #endregion
+
+            #region Question 9
+            // Given int pages = 464;
+            // and bool isAvailable = true;,
+            // print "You can borrow this book" only if pages is greater than 300 and isAvailable is true.
+            // Use the && operator.
+
+            //int pages = 464;
+            //bool isAvailable = true;
+
+            //if (pages > 300 && isAvailable)
+            //{
+            //    Console.WriteLine("You can borrow this book");
+            //}
+            #endregion
         }
     }
 }
