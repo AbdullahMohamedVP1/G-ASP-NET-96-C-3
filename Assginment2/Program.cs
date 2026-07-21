131e6a540514c4eef1acbdc54f88de2c827a07b2
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-07-21 14:05:04 +0300

    Question1

--- Assginment2/Program.cs
+++ Assginment2/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question1
+            //Declare string title = "clean code";.
+            //Call title.ToUpper() and store it in a new variable upperTitle.
+            //Print both title and upperTitle to show that title did not change
+
+            //string title = "clean code";
+            //string UpperTitle = title.ToUpper();
+            //Console.WriteLine($"normal: {title}");
+            //Console.WriteLine($"Upper: {UpperTitle}");
+
+            #endregion
         }
     }
 }
