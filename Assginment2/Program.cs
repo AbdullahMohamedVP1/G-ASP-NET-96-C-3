1c70f4ef0924ab1da5bbcbe89169f42a774556a1
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-07-21 14:46:55 +0300

    Question15

--- Assginment2/Program.cs
+++ Assginment2/Program.cs
@@ -199,6 +199,22 @@ namespace Assginment2
             //while (counter <= 3);
             #endregion
 
+            #region Question 15
+            // Using the same books array, use a foreach loop to print every book title.
+
+            //string[] books =
+            //{
+            //    "Clean Code",
+            //    "The Pragmatic Programmer",
+            //    "Refactoring"
+            //};
+
+            //foreach (string book in books)
+            //{
+            //    Console.WriteLine(book);
+            //}
+            #endregion
+
         }
     }
 }
