67677d1fd798294771807a70a0c3cf10ad34dfda
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-07-21 14:38:29 +0300

    Question10

--- Assginment2/Program.cs
+++ Assginment2/Program.cs
@@ -121,6 +121,28 @@ namespace Assginment2
             //    Console.WriteLine("You can borrow this book");
             //}
             #endregion
+
+            #region Question 10
+            // Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
+
+
+            //string title = "Refactoring";
+
+            //switch (title)
+            //{
+            //    case "Clean Code":
+            //        Console.WriteLine("Great choice!");
+            //        break;
+
+            //    case "Refactoring":
+            //        Console.WriteLine("Nice pick!");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Never heard of it");
+            //        break;
+            //}
+            #endregion
         }
     }
 }
